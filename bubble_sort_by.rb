991e5b68916dd27a2a6b83e1e4ab9ef53e4bd5e5
ericmbouwe <ericmbouwe@yahoo.fr>
2020-04-08 11:52:45 +0100

    bubble_sort_by refactored

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,13 +1,13 @@
 def bubble_sort_by(arr)
   loop do
-    sorted = true
+    sorted = false
     (0...arr.length - 1).each do |i|
       if yield(arr[i], arr[i + 1]).positive?
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
-        sorted = false
+        sorted = true
       end
     end
-    break if sorted
+    break if !sorted
   end
   arr
 end
@@ -15,5 +15,6 @@ end
 var = bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
 end
+
 
 print var
